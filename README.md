f440955fabf26f4d4e26cff0570819c266c0c486
Steven Schweiger <sschweig90@gmail.com>
2023-09-15 05:11:39 -0400

    revert overlay changes

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-This is a back end for the books api interface.
+# My Library
+This is a demonstration of Google's Books API. This is a simple frontend designed
+to allow a user to interact with the API's search functionality and browse for 
+books by keyword.
 
 ## Getting Started
 
@@ -13,3 +16,9 @@ pnpm dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+## TODO
+Clean up Tailwind
+Implement pop up on mouseover for in depth view
+Advanced query filters
+Style options
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,28 +17,22 @@ export default function Dash() {
         .then(json => setLibrary(json));
   }
 
-  const popOver = (evt: MouseEvent<HTMLButtonElement>, book: BookType) => {
-    return 
-  }
-
   const BookList = () => {
     return <div className="h-screen bg-green-200 grid grid-cols-2 md:grid-cols-3 gap-4">  
         {library.map(book => (<BookCard key={book.uid} {...book}/>))}
     </div>
   }
-  
+
   const BookCard = (book: BookType) => {
     return <>
-      <button onMouseOver={(evt: MouseEvent<HTMLButtonElement>)=> popOver(evt, book)} className="h-80 rounded-lg bg-no-repeat content flex flex-wrap overflow-hidden py-2 pb-4">
-        <div className="bg-gray-900 bg-opacity-50 hover:opacity-75 rounded-lg bg-no-repeat content flex flex-wrap overflow-hidden py-2 pb-4" >
+      <button className="h-80 rounded-lg bg-no-repeat content flex flex-wrap overflow-hidden py-2 pb-4">
+        <div className="bg-gray-900 bg-opacity-50 hover:opacity-70 rounded-lg bg-no-repeat content flex flex-wrap overflow-hidden py-2 pb-4" >
           <img src={book.imagePreview} className="pl-3 h-1/2 w-1/4"/>
           <div className="item-body px-2 w-2/3 h-1/2 break-all min-h-fit">
-            <h1 className="text-xl font-bold min-h-fit">{book.title}</h1>
-            <h3 className="text-lg italic min-h-fit">{book.authors}</h3>
+            <h1 className="text-xl font-bold min-h-fit text-wrap">{book.title}</h1>
+            <h3 className="text-lg italic min-h-fit text-wrap">{book.authors}</h3>
           </div>
-          <div className="item-body px-2 h-1/2">
-            <p className="text-m overflow-hidden min-h-fit pt-3">{book?.description}</p>
-          </div>
+          <div><p className='overflow-hidden pl-2 pr-2 pt-2 pb-2'>{book.description}</p></div>
         </div>
       </button>
     </>
